904b9db1a17138ec81dadf8f7b5bfddfa83fec60
Gabriel Ratajczak <gabirat@onet.pl>
2018-06-06 10:31:40 +0200

    fixed timer script

--- js/timer.js
+++ js/timer.js
@@ -1,26 +1,22 @@
-const NEW_SEASON_DATE = new Date("01-07-2018");
-const CURRENT_TIME = new Date();
-const COUNTDOWN_TIME = NEW_SEASON_DATE - CURRENT_TIME;
+const NEW_SEASON_DATE = new Date("2018-07-01");
+let COUNTDOWN_TIME = new Date ( NEW_SEASON_DATE - new Date() );
 
 const daysDOM = document.querySelector("#days"); 
 const hoursDOM = document.querySelector("#hours"); 
 const minutesDOM = document.querySelector("#minutes"); 
 const secondsDOM = document.querySelector("#seconds");
-//TODO: FIX IT
+
 window.onload = () => {
     let d,h,m,s;
-    d = COUNTDOWN_TIME.getDate();
-    h = COUNTDOWN_TIME.getHours();
-    m = COUNTDOWN_TIME.getMinutes();
-    s = COUNTDOWN_TIME.getSeconds();
     setInterval(() => {
-        daysDOM.innerHTML = d;
-        hoursDOM = h;
-        minutesDOM = m;
-        secondsDOM = s;
-        d--;
-        h--;
-        m--;
-        s--;
+        d = COUNTDOWN_TIME.getDate();
+        h = COUNTDOWN_TIME.getHours();
+        m = COUNTDOWN_TIME.getMinutes();
+        s = COUNTDOWN_TIME.getSeconds();
+        daysDOM.innerHTML = ( (d < 10) ? `0${d}` : d ) ;
+        hoursDOM.innerHTML = ( (h < 10) ? `0${h}` : h ) ;;
+        minutesDOM.innerHTML = ( (m < 10) ? `0${m}` : m ) ;;
+        secondsDOM.innerHTML = ( (s < 10) ? `0${s}` : s ) ;;
+        COUNTDOWN_TIME = new Date ( NEW_SEASON_DATE - new Date() );
     }, 1000);
 };
